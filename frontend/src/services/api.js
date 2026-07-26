069a561a22724e083da6d97968d6d37e4a99f862
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-07-26 13:07:38 -0300

    Troca da baseUrl na api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000', // Retirada a barra do final
+    baseURL: import.meta.env.VITE_API_URL || 'https://sistema-de-enquetes-backend.vercel.app',
 });
 
 api.interceptors.request.use((config) => {
