81533e8c588a8c444b97387b3f8fd04d6c58aaa1
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-07-26 13:42:17 -0300

    Correção no nome da db

--- backend/src/config/Database.php
+++ backend/src/config/Database.php
@@ -9,7 +9,7 @@ class Database {
     public static function getConnection() {
         $host   = $_ENV['DB_HOST']   ?? getenv('DB_HOST')   ?: 'mysql-1b29cb70-ajenteduplo000-ad7d.l.aivencloud.com';
         $port   = $_ENV['DB_PORT']   ?? getenv('DB_PORT')   ?: '25167';
-        $dbname = $_ENV['DB_NAME']   ?? getenv('DB_NAME')   ?: 'padrãodb';
+        $dbname = $_ENV['DB_NAME']   ?? getenv('DB_NAME')   ?: 'defaultdb';
         $user   = $_ENV['DB_USER']   ?? getenv('DB_USER')   ?: 'avnadmin';
         $pass   = $_ENV['DB_PASS']   ?? getenv('DB_PASS')   ?: 'SUA_SENHA';
 
