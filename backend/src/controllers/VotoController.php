352eb4e6f58d9f118ccf33e4e89e2f9c9db0c6f0
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-07-26 13:50:48 -0300

    inicialização do votocontrollers

--- backend/src/controllers/VotoController.php
+++ backend/src/controllers/VotoController.php
@@ -4,7 +4,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-class EnqueteController {
+class VotoController {
 
     // POST /enquetes/vote - Computar Voto
     public function vote() {
